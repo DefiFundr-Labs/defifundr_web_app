88ead193e882e787f407dc0392c805dfe8d1bed6
pvsaint <vpeter817@gmail.com>
2025-02-23 10:51:21 +0100

    feat:complete Business Details Capture Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
-import "./App.css"
-import businessDetailsScreen from "./pages/BusinessDetailsScreen"
 
 function App() {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/business/:id", // This allows for dynamic business IDs
+        path: "/business", 
         element: <BusinessDetailsScreen />,
       },
     ],
